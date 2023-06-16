d45029c6091bf784c2a70d7a8d9da3cfbeff52b0
Pereverziev Andrii <a.pereverziev@enamine.net>
2023-06-16 16:14:41 +0300

    Initial project

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Kvitka Hub</title>
+
+    </head>
+    <body>
+        <h1>Kvitka Hub</h1>
+    </body>
+</html>
